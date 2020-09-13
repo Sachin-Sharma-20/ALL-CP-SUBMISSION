b70c0321ed43206835855af2b0a07c979c637da9
Sachin-Sharma-20 <65070633+Sachin-Sharma-20@users.noreply.github.com>
2020-09-13 23:26:37 +0530

    ATCODER

---
+++ A_NOT.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector <int> vi;
+typedef set <int> se;
+#define endl "\n"
+#define for(i,a,b) for(int i = a;i<b;i++)
+#define all(v) v.begin(),v.end()
+#define ff first
+#define ss second
+#define ll long long
+#define mod 1000000007
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+void solve(){
+    int x;
+    cin>>x;
+    if(x==1) cout<<0<<endl;
+    else if(x==0) cout<<1<<endl;
+
+
+}
+ 
+ 
+int main()
+{
+solve();
+ 
+return 0;
+}
